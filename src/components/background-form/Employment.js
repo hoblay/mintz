555e98abe8a64b0ee784492d2106f142a2bf8340
Winslet Mateus <hoblayrecords@gmail.com>
2021-04-30 17:56:56 +0100

    Employment Yes view

--- src/components/background-form/Employment.js
+++ src/components/background-form/Employment.js
@@ -24,15 +24,269 @@ const Employment = () => {
       </p>
       <div className="card p-4">
         <h4 className="mb-4">Current Status</h4>
-        <div className="d-flex flex-column w-50 mx-auto">
+        <div className="row">
+          <div className="col-md-6">
+            <button
+              className="btn btn-primary btn-block w-100 text-uppercase py-3"
+              onClick={() => setForm(true)}
+            >
+              I am currently employed
+            </button>
+          </div>
+          <div className="col-md-6">
+            <button
+              className="btn btn-secondary btn-block w-100 text-uppercase py-3"
+              onClick={() => setForm(false)}
+            >
+              I am currently unemployed
+            </button>
+          </div>
+        </div>
+      </div>
+      <hr />
+      <div className={`card p-4 mt-4 ${!form && 'd-none'}`}>
+        <h4 className="mb-4">Current Employment</h4>
+
+        <div className="row col-12">
+          <div className=" col-md-8">
+            <label>
+              Employer or Company Name
+              <span className="text-danger">*</span>
+            </label>
+            <input type="text" className="form-control" />
+          </div>
+          <div className="col-md-4">
+            <label>
+              Supervisor Name
+              <span className="text-danger">*</span>
+            </label>
+            <input type="text" className="form-control" />
+          </div>
+        </div>
+        <div className="row col-12 mt-4">
+          <div className="col-md-4">
+            <label>
+              Your Position
+              <span className="text-danger">*</span>
+            </label>
+            <input type="text" className="form-control" />
+          </div>
+          <div className="col-md-4">
+            <label>
+              Start Date (YYYY-MM)
+              <span className="text-danger">*</span>
+            </label>
+            <input type="text" className="form-control" />
+          </div>
+        </div>
+        <div className="row col-12 mt-4">
+          <div className="col-md-4">
+            <label>
+              City
+              <span className="text-danger">*</span>
+            </label>
+            <input type="text" className="form-control" />
+          </div>
+          <div className="col-md-4">
+            <label>
+              Country
+              <span className="text-danger">*</span>
+            </label>
+            <select class="form-select" aria-label="Default select example">
+              <option value="1">Select</option>
+              <option value="1">Option 1</option>
+              <option value="2">Option 2</option>
+            </select>
+          </div>
+          <div className="col-md-4">
+            <label>
+              Province/State
+              <span className="text-danger">*</span>
+            </label>
+            <select class="form-select" aria-label="Default select example">
+              <option value="1">Select</option>
+              <option value="1">Option 1</option>
+              <option value="2">Option 2</option>
+            </select>
+          </div>
+        </div>
+        <div className="row col-12 mt-4">
+          <div className="col-md-4">
+            <label>
+              Phone Number
+              <span className="text-danger">*</span>
+            </label>
+            <input type="text" className="form-control" />
+          </div>
+          <div className="col-md-2">
+            <label>
+              Ext
+              <span className="text-danger">*</span>
+            </label>
+            <input type="text" className="form-control" />
+          </div>
+          <div className="col-md-6 py-3">
+            <label className="d-block">
+              May we contact your current employer?{' '}
+            </label>
+            <div className="form-check d-inline-block me-3">
+              <input
+                type="radio"
+                className="form-check-input"
+                id="defaultUnchecked"
+                name="defaultExampleRadios"
+              />
+              <label className="form-check-label" for="defaultUnchecked">
+                Yes
+              </label>
+            </div>
+            <div className="form-check d-inline-block">
+              <input
+                type="radio"
+                className="form-check-input"
+                id="defaultUnchecked"
+                name="defaultExampleRadios"
+              />
+              <label className="form-check-label" for="defaultUnchecked">
+                No
+              </label>
+            </div>
+          </div>
+        </div>
+        <div className="row col-4 mt-4 p-2">
+          <button className="btn btn-primary btn-block w-100 text-uppercase py-3">
+            ADD A COMMENT
+          </button>
+        </div>
+      </div>
+      <hr />
+      <div className="card p-4 mt-4">
+        <h4 className="mb-4">Previous Employment</h4>
+
+        <div className="row col-12">
+          <div className=" col-md-8">
+            <label>
+              Employer or Company Name
+              <span className="text-danger">*</span>
+            </label>
+            <input type="text" className="form-control" />
+          </div>
+          <div className="col-md-4">
+            <label>
+              Supervisor Name
+              <span className="text-danger">*</span>
+            </label>
+            <input type="text" className="form-control" />
+          </div>
+        </div>
+        <div className="row col-12 mt-4">
+          <div className="col-md-4">
+            <label>
+              Your Position
+              <span className="text-danger">*</span>
+            </label>
+            <input type="text" className="form-control" />
+          </div>
+          <div className="col-md-4">
+            <label>
+              Start Date (YYYY-MM)
+              <span className="text-danger">*</span>
+            </label>
+            <input type="text" className="form-control" />
+          </div>
+        </div>
+        <div className="row col-12 mt-4">
+          <div className="col-md-4">
+            <label>
+              City
+              <span className="text-danger">*</span>
+            </label>
+            <input type="text" className="form-control" />
+          </div>
+          <div className="col-md-4">
+            <label>
+              Country
+              <span className="text-danger">*</span>
+            </label>
+            <select class="form-select" aria-label="Default select example">
+              <option value="1">Select</option>
+              <option value="1">Option 1</option>
+              <option value="2">Option 2</option>
+            </select>
+          </div>
+          <div className="col-md-4">
+            <label>
+              Province/State
+              <span className="text-danger">*</span>
+            </label>
+            <select class="form-select" aria-label="Default select example">
+              <option value="1">Select</option>
+              <option value="1">Option 1</option>
+              <option value="2">Option 2</option>
+            </select>
+          </div>
+        </div>
+        <div className="row col-12 mt-4">
+          <div className="col-md-4">
+            <label>
+              Phone Number
+              <span className="text-danger">*</span>
+            </label>
+            <input type="text" className="form-control" />
+          </div>
+          <div className="col-md-2">
+            <label>
+              Ext
+              <span className="text-danger">*</span>
+            </label>
+            <input type="text" className="form-control" />
+          </div>
+          <div className="col-md-6 py-3">
+            <label className="d-block">
+              May we contact your current employer?{' '}
+            </label>
+            <div className="form-check d-inline-block me-3">
+              <input
+                type="radio"
+                className="form-check-input"
+                id="defaultUnchecked"
+                name="defaultExampleRadios"
+              />
+              <label className="form-check-label" for="defaultUnchecked">
+                Yes
+              </label>
+            </div>
+            <div className="form-check d-inline-block">
+              <input
+                type="radio"
+                className="form-check-input"
+                id="defaultUnchecked"
+                name="defaultExampleRadios"
+              />
+              <label className="form-check-label" for="defaultUnchecked">
+                No
+              </label>
+            </div>
+          </div>
+        </div>
+        <div className="row col-4 mt-4 p-2">
+          <button className="btn btn-primary btn-block w-100 text-uppercase py-3">
+            ADD A COMMENT
+          </button>
+        </div>
+      </div>
+      <div className="row mt-4 mb-4">
+        <div className="col-md-6">
           <button
-            className="btn btn-primary mb-3 text-uppercase py-2"
+            className="btn btn-secondary btn-block w-100 text-uppercase py-3"
             onClick={() => setForm(true)}
           >
-            I am currently employed
+            Add previous employment
           </button>
-          <button className="btn btn-secondary text-uppercase mb-3">
-            I am currently unemployed
+        </div>
+        <div className="col-md-6">
+          <button className="btn btn-secondary btn-block w-100 text-uppercase py-3">
+            Add unemployment period
           </button>
         </div>
       </div>
